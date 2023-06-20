64b8abc574773d9d1b9d742cc9ab98a9d58eb5ff
John Pham <phambinouh@gmail.com>
2023-06-20 08:39:05 -0500

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,10 +1,18 @@
 Write-Host "Starting John's automation script!"
 # $SearchItem = Read-Host -Prompt "Enter an Asset Tag or Serial Number"
-$SearchItem = "CTS1337"
+$SearchItem = "CTS34894"
+
+### Need to find a way to connect to Excel and obtain the information below automatically.
+$SerialNumber = "1BMYLL3"
+$AssetStatus = "Deployed"
+$Model = "Dell Latitude 3310 i3"
+$PrimaryUser = "Pham, John"
+$Location = "N/A"
+$LocationDetails = "N/A"
 
 $Driver = Start-SeEdge
-Enter-SeUrl https://navigator.americannational.com/ -Driver $Driver
 
+Enter-SeUrl https://navigator.americannational.com/ -Driver $Driver
 
 $Element = Find-SeElement -Driver $Driver -Id "global-search__header__form__input_id"
 Send-SeKeys -Element $Element -Keys "$SearchItem"
@@ -14,3 +22,10 @@ Send-SeKeys -Element $Element -Keys "$SearchItem"
 
 $Element = Find-SeElement -Driver $Driver -Id "checkAllClassFilter"
 Invoke-SeClick -Element $Element
+
+Write-Host "Waiting for the Hardware Asset to appear. (5 seconds)"
+Start-Sleep -Seconds 5
+
+$Element = Find-SeElement -Driver $Driver -ClassName "results-details-highlight"
+Invoke-SeClick -Element $Element
+
